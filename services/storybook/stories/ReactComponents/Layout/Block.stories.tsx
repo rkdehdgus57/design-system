0a1ba24558b55affb8561a8e2a8cd078ea1ffa39
DongHyun Kang <rkdehdgus57@gmail.com>
2025-01-31 01:52:52 +0900

    storybook 설치 및 설정

---
+++ services/storybook/stories/ReactComponents/Layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import "@fastcampus/react-components-layout/styles.css";
+import { Block as _Block } from "@fastcampus/react-components-layout";
+
+export default {
+  title: "ReactComponents/Layout/Block",
+  component: _Block,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const BlockStory = {};
